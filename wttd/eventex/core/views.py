531c70af459bf060490019862a338fed59a329ef
Dell <delsantos@hotmail.com.br>
2015-08-09 13:29:56 -0300

    modificando forma de implementar o static no context

--- wttd/eventex/core/views.py
+++ wttd/eventex/core/views.py
@@ -1,13 +1,20 @@
 # coding: utf-8
 #from django.http import HttpResponse
 #from django.template import loader, Context
+
 from django.shortcuts import render_to_response
-from django.conf import settings
+#from django.conf import settings
+
+from django.template import RequestContext
 
 def homepage( request ):
 	#t = loader.get_template( 'index.html' )
 	#c = Context()
 	#content = t.render( c )
 	#return HttpResponse( content )
-	context = {'STATIC_URL':settings.STATIC_URL}
+
+	#context = {'STATIC_URL':settings.STATIC_URL}
+
+	context = RequestContext( request )
 	return render_to_response('index.html', context)
+
