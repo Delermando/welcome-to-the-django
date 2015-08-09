171d0a6b3716f5b05778728d59f1ff3753df2226
Dell <delsantos@hotmail.com.br>
2015-08-09 11:54:48 -0300

    redenrizando template

--- wttd/eventex/core/views.py
+++ wttd/eventex/core/views.py
@@ -1,6 +1,10 @@
 # coding: utf-8
 from django.http import HttpResponse
+from django.template import loader, Context
 
 def homepage( request ):
-	return HttpResponse('Bem Vindo ao EventeX!')
+	t = loader.get_template( 'index.html' )
+	c = Context()
+	content = t.render( c )
+	return HttpResponse( content )
 
