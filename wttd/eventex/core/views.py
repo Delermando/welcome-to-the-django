c2e4e9982d855f037189cec094679a3aebb46813
Dell <delsantos@hotmail.com.br>
2015-08-09 13:24:06 -0300

    aplicando path do static

--- wttd/eventex/core/views.py
+++ wttd/eventex/core/views.py
@@ -2,9 +2,12 @@
 #from django.http import HttpResponse
 #from django.template import loader, Context
 from django.shortcuts import render_to_response
+from django.conf import settings
+
 def homepage( request ):
 	#t = loader.get_template( 'index.html' )
 	#c = Context()
 	#content = t.render( c )
 	#return HttpResponse( content )
-	return render_to_response('index.html')
+	context = {'STATIC_URL':settings.STATIC_URL}
+	return render_to_response('index.html', context)
