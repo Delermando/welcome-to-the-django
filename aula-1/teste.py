702e127a16b19d78e97af9e1d84ab74a713dfcff
Delermando <delsantos@hotmail.com.br>
2015-09-16 23:14:40 -0300

    comentarios sobre a aula

---
+++ aula-1/teste.py
@@ -0,0 +1,28 @@
+help('modules') #mostra as bibliotecas padroes do python
+
+
+import os
+os.getcwd() #mostra o diretorio corrent
+
+print(' - ' * 1000) #printa a string quantas vezes forem definidas
+
+count = 0
+count += 1
+
+food = ['oranges', 'aples','cats']
+
+for i in food:
+	print(('-->' + i ) * 100)
+
+
+for i in range(1, 11):
+	print i
+
+for i in range(1, 11, 2):
+	print i
+
+#string de multiplas linhas
+'''teste
+teste
+teste'''
+
